c23247eeb69acb44114c2e32dddacf319f20447e
tejaswinigadge <tejaswinigadge55@gmail.com>
2020-05-16 10:19:21 +0530

    initial commit

---
+++ student-restful-web-service/src/main/java/com/in28minutes/studentrestfulwebservice/StudentRestfulWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.studentrestfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRestfulWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRestfulWebServiceApplication.class, args);
+	}
+
+}
---
+++ student-restful-web-service/src/test/java/com/in28minutes/studentrestfulwebservice/StudentRestfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.studentrestfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRestfulWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
