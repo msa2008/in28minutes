644d74e33e98ea8274d14a4296cda6ec1b85fdae
tejaswinigadge <tejaswinigadge55@gmail.com>
2020-05-19 18:11:35 +0530

    last commit

--- limits-service/src/main/java/com/in28minutes/mircoservices/limitsservice/controller/LimitConfigurationController.java
+++ limits-service/src/main/java/com/in28minutes/mircoservices/limitsservice/controller/LimitConfigurationController.java
@@ -1,6 +1,7 @@
 package com.in28minutes.mircoservices.limitsservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import com.in28minutes.mircoservices.limitsservice.model.Configuration;
 import com.in28minutes.mircoservices.limitsservice.model.LimitConfiguration;
 
 @RestController
+@RefreshScope
 public class LimitConfigurationController {
 	@Autowired
 	Configuration configuration;
